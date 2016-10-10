bb941a39f9e757a236695eea57edd50a827cb757
Vu Van Tiep <vuvantiep@Vus-Mac-mini.local>
2016-10-10 17:41:33 +0700

    first commit

---
+++ Card/Assets/AssetsGame/4.Scripts/Config.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+public class Config{
+
+	public static string ipAddressServer = "127.0.0.1";
+	public static int port = 12000;
+	public static SocketType type = SocketType.Stream; //TCP
+	public static ProtocolType protocolType = ProtocolType.Tcp;
+	public static bool isBlocking = true;
+}
